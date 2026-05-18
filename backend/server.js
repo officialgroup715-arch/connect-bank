c31eaedabdb23488a0797bd7697d4992530b047f
officialgroup715-arch <officialgroup715@gmail.com>
2026-05-18 04:55:45 -0700

    fix bugs on backend

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,27 @@ import userRouter from "./router/auth.js"
 import cors from "cors"
 
 const app = express()
-dotenv.config()
-console.log(process.env.CLIENT_URL);
-app.use(cors({
-  origin: [process.env.CLIENT_URL, "http://localhost:5173"],
-   methods: ["GET", "POST", "PUT", "DELETE"],
-   allowedHeaders: ["Content-Type", "Authorization"]
+dotenv.config() 
 
-}))
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.CLIENT_URL,
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 
 const PORT = process.env.PORT || 8000
 app.get("/", (req, res) => {
